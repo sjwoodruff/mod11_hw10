f934a0c081800c3df99d6a0c806c6976743ac5aa
Sterling Woodruff <sterlingwoodruff@mail.weber.edu>
2017-04-12 23:28:00 +0000

    initial commit

---
+++ 0001010_hw10_task2.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  0001010_hw10_task2.c
+ *       	Usage:  ./0001010_hw10_task2.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/12/2017 11:26:39 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Sterling Woodruff (), sterlingwoodruff@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+/* Function Prototypes */
+
+/* Main Program */
+int main (int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
---
+++ 0001010_hw10_task3.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  0001010_hw10_task3.c
+ *       	Usage:  ./0001010_hw10_task3.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/12/2017 11:26:53 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Sterling Woodruff (), sterlingwoodruff@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+/* Function Prototypes */
+
+/* Main Program */
+int main (int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
